d8c64e94490f353d08417890cf66c6fd92dedcb1
jheimc <jheimymiranda@gmail.com>
2021-12-12 13:07:49 -0400

    responsive and print report

--- src/app/pages/report-income-page/report-income-page.component.ts
+++ src/app/pages/report-income-page/report-income-page.component.ts
@@ -1,20 +1,28 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
+import { ITable, Item, PdfMakeWrapper, Table, Txt } from 'pdfmake-wrapper';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 import { RequestService } from 'src/app/services/request.service';
-
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+type TableRow=[any,any,any,any,any,any,any,any,any,any,any,any,any,any]
 @Component({
   selector: 'app-report-income-page',
   templateUrl: './report-income-page.component.html',
-  styleUrls: ['./report-income-page.component.css']
+  styleUrls: ['./report-income-page.component.css'],
+  providers: [DatePipe]
 })
 export class ReportIncomePageComponent implements OnInit {
 
   constructor(
     public dialog: MatDialog, 
     private RequestService: RequestService,
-    private snack:MatSnackBar,) { }
+    private snack:MatSnackBar,
+    private datePipe: DatePipe
+    ) { }
  
   usersResponse:any;
   user:any;
@@ -94,4 +102,59 @@ export class ReportIncomePageComponent implements OnInit {
     
         return this.dataSource?.data.map(t => t[field]).reduce((acc, value) => acc + value, 0);
   }
+  async printReport(){
+    let myDate = new Date();
+    let myActualDate = this.datePipe.transform(myDate, 'yyyy-MM-dd');
+        const pdf=new PdfMakeWrapper();
+        pdf.pageMargins([0,20,0,0])
+        pdf.pageSize('A4')
+        pdf.pageOrientation('landscape')
+        pdf.defaultStyle({
+          fontSize:11,
+          //font:'roboto'
+        })
+       
+          /* pdf.add(new Txt('SISTEMA DE GESTION DE ALMACENES').margin([20,0]).bold().fontSize(13).end);
+          pdf.add(new Txt(this.configurations.nameForReport).margin([20,0]).bold().fontSize(13).end);
+          pdf.add(new Txt('Teléfono: '+this.configurations.telephoneForReport).margin([20,0]).fontSize(10).end);
+          pdf.add(new Txt('Email: '+this.configurations.emailForReport).margin([20,0]).fontSize(10).end);
+          pdf.add(new Txt('Dirección: '+this.configurations.addresForReport).margin([20,0]).fontSize(10).end);
+ */
+          pdf.add(new Txt('REPORTE DE INGRESOS').bold().alignment('center').fontSize(13).end);
+          pdf.add(new Txt('Gestión '+this.yearSelected).bold().alignment('center').fontSize(12).end);
+          pdf.add(pdf.ln(1));
+          pdf.add(new Txt('Fecha: '+ myActualDate).margin([20,10]).relativePosition(550,-32).end);
+          pdf.add(new Txt('los ingresos que se detallan a continuacion son de proposito informativo')
+            .margin([20,10]).fontSize(10).end);
+          
+            pdf.add(this.createTable(this.dataSource.data))
+         
+           pdf.create().open();
+        
+    }
+
+    //   
+
+
+    createTable(data: Item[]):ITable{
+      [{}]
+      var foot=['Total General',this.getTotalCost('Enero'),this.getTotalCost('Febrero'),this.getTotalCost('Marzo'),this.getTotalCost('Abril'),this.getTotalCost('Mayo'),this.getTotalCost('Junio'),this.getTotalCost('Julio'),this.getTotalCost('Agosto')
+      ,this.getTotalCost('Septiembre'),this.getTotalCost('Octubre'),this.getTotalCost('Noviembre'),this.getTotalCost('Diciembre'),this.getTotalCost('Totales')]
+        return new Table([
+        [ 'CUENTA', 'ENERO','FEBRERO','MARZO','ABRIL','MAYO','JUNIO','JULIO','AGOSTO','SEPTIEMBRE','OCTUBRE','NOVIEMBRE','DICIEMBRE','TOTAL GENERAL'],
+        ...this.extractData(data),foot,
+      ]).margin([20,10]).alignment('center').fontSize(10).layout({hLineColor:(rowIndex:number,node:any,columnIndex:number)=>{
+              return  '#c2c2c2';
+      },vLineColor:((rowIndex:number,node:any,columnIndex:number)=>{
+          return  '#c2c2c2';
+      }),fillColor:((rowIndex:number,node:any,columnIndex:number)=>{
+          return  rowIndex===0?'#c2c2c2':'';
+      })
+      }).end;
+    }
+  
+    extractData(data:any[]):TableRow[]{
+        var index=1
+        return data.map(row=>[row.accountName,row.Enero,row.Febrero,row.Marzo,row.Abril,row.Mayo,row.Junio,row.Julio,row.Agosto,row.Septiembre,row.Octubre,row.Noviembre,row.Diciembre,row.Totales])
+    } 
 }
